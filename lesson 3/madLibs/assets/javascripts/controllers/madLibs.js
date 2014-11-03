827c8bd6a28153e45a98b0622fa9663f8e06a209
Pablo Cordero <pablocordero@gmail.com>
2014-11-02 19:51:39 -0600

    Refactor madLibs using a controller

--- lesson 3/madLibs/assets/javascripts/controllers/madLibs.js
+++ lesson 3/madLibs/assets/javascripts/controllers/madLibs.js
@@ -1,1 +1,28 @@
-madLibs
+angular.module('madLibs', [])
+  .controller('keywordsCtrl',
+    function($scope){
+      $scope.gender = "female";
+      $scope.sheHe = "she";
+      $scope.herHis = "her"
+      $scope.keywords = { 
+        name: "",
+        jobTitle: "",
+        tediousTask: "",
+        dirtyTask: "",
+        celebrity: "",
+        uselessSkill: "",
+        obnoxiousCelebrity: "",
+        hugeNumber: "",
+        adjective: ""
+      };
+      $scope.updateCopy = function(gender) {
+        if(gender === 'female'){
+          $scope.sheHe = "she";
+          $scope.herHis = "her";
+        } else {
+          $scope.sheHe = "he";
+          $scope.herHis = "his";
+        }
+
+      }
+    });
